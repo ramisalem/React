07d0b855b8fd0398447e87bdce851c30077847eb
rami <rami@rami.com>
2018-08-20 22:02:24 +0530

    starting point

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import  css from './footer.css';
+const Footer = (props) => (
+    <div className= {css.footer}>
+    Made with  by @glenmaddern, @mxstbr, @_philpl‬ & @probablyup
+      </div>
+);
+
+export default Footer;
